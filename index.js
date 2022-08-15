ae6075d3f63f758f30967c545a128141eaf68f75
SAMUEL KOBINA GYASI <gyasisamuelkobina22@gmail.com>
2022-08-15 18:52:54 +0100

    sup website

---
+++ index.js
@@ -0,0 +1,10 @@
+var mobileMenu = document.getElementById('mobile-menu');
+
+
+function openMenu() {
+    mobileMenu.style.right = "0"
+}
+
+function closeMenu() {
+    mobileMenu.style.right = "-220px"
+}
